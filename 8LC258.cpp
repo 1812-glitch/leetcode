8443b5c1820e9b5a82514b6b22abe4c8a033ff1c
1812-glitch <hr3645229@gmail.com>
2025-07-19 14:52:24 +0530

    LeetCode Problem 258

---
+++ 8LC258.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) 
+    {
+        if (num <= 0) return 0;
+
+        while(num>9)
+        {
+            int ls = num%10;
+            int a = ls;
+            num = num/10 + ls;
+        }
+        return num;
+    }
+};
+
+int main()
+{
+    Solution a;
+    cout << a.addDigits(55);
+    return 0;
+}
