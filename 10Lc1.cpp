db7ca23f74f72aa813da17be70eb44440b11102c
1812-glitch <hr3645229@gmail.com>
2025-07-25 11:28:20 +0530

    LeetCode Problem 1

---
+++ 10Lc1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        for(int i = 0; i < nums.size(); i++) 
+        {
+            for (int j = i + 1; j < nums.size(); j++) 
+            {
+                if (nums[i] + nums[j] == target) 
+                {
+                    return {i, j};
+                }
+            }
+        }
+         return {};
+    }
+};
+
+int main()  // ✅ fixed typo
+{
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 110;
+    vector<int> result = s.twoSum(nums, target);
+    
+    // ✅ print vector contents
+    for (int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
