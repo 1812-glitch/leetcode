666bdb10f7eda96bc9e3f1af0abe6bc3fe98481a
1812-glitch <hr3645229@gmail.com>
2025-08-30 22:02:52 +0530

    LeetCode Problrm 509

---
+++ 26LC509.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.fib(2) << endl;
+    cout << s.fib(3) << endl;
+    cout << s.fib(4) << endl;
+}
