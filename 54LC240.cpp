67c712501a7b00f2f3f643be78492a32d0b018d5
1812-glitch <hr3645229@gmail.com>
2026-03-11 08:11:45 +0530

    LeetCode Problem 240

---
+++ 54LC240.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+ bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+      int n = matrix.size();
+      int m = matrix[0].size();
+      int row = 0, col = m - 1;
+        while(row < n && col >= 0)
+        {
+            if(matrix[row][col] == target)
+            return true;
+            else if(matrix[row][col] < target)
+            row++;
+            else
+            col--;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15},
+                                  {2, 5, 8, 12, 19},
+                                  {3, 6, 9, 16, 22},
+                                  {10,13,14,17,24},
+                                  {18,21,23,26,30}};
+    int target = 14;
+    bool found = obj.searchMatrix(matrix, target);
+    cout<<found<<endl;
+    return 0;
+}
